475686ed13b1f9cda2c9e109f54fb5ade6b04b1a
WistonThiago <wthyago-2000@hotmail.com>
2024-04-09 18:00:56 -0300

    Adição da resolução com loop while.

--- Atividade_1.py
+++ Atividade_1.py
@@ -1,4 +1,17 @@
+print("Usando o loop FOR: ")
+#Using FOR
 for i in range (20, 13, -1):
     print("ANDAR: ", i)
 for i in range (12, 0, -1):
     print("ANDAR: ", i)
+print("===================")
+print("Usando o loop WHILE: ")
+#Using WHILE
+counter = 20
+while (counter >= 1):
+    if (counter == 13):
+        counter -= 1
+        continue
+    else:
+        print ("ANDAR: ", counter)
+        counter -= 1
